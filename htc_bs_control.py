d631915ae8b4d875a726f2381e7efd5f8e8390e1
Julia <MissingNO123@users.noreply.github.com>
2023-10-31 16:07:58 -0400

    Send commands concurrently, which is way faster

--- htc_bs_control.py
+++ htc_bs_control.py
@@ -35,36 +35,44 @@ v2_Power_Char = "00001525-1212-efde-1523-785feabcd124"
 CMD_ON_V2 = b"\x01"
 CMD_OFF_V2 = b"\x00"
 
-async def powerState(bs, value):
+
+async def powerState(bs, state):
     try:
         async with BleakClient(bs) as client:
-            print("~%s~" % bs)
-            print("Connecting...")
+            print("~ Connecting to %s... ~" % bs)
             while client.is_connected is False:
                 time.sleep(0.05)
             print("Connected!")
-            print("Powering %s..." % ("ON " if PowerState is True else "OFF"))
-            try: 
-                cmd = CMD_ON_V2 if value else CMD_OFF_V2
-                p = await client.read_gatt_char(v2_Power_Char) #check if V2 BS Power Characteristic is available
+            print("Powering %s..." % ("ON" if PowerState else "OFF"))
+            # check if V2 BS Power Characteristic is available (by trying to write to it)
+            try:
+                cmd = CMD_ON_V2 if state else CMD_OFF_V2
+                p = await client.read_gatt_char(v2_Power_Char)
                 p = await client.write_gatt_char(v2_Power_Char, cmd)
-            except BleakError: #if V2 Power Characteristic is not available that means it"s probably a V1 BS
-                try: 
-                    cmd = CMD_ON if value else CMD_OFF
+            # if V2 Power Characteristic is not available that means it's probably a V1 BS
+            except BleakError:  
+                try:
+                    cmd = CMD_ON if state else CMD_OFF
                     p = await client.read_gatt_char(v1_Power_Char)
                     p = await client.write_gatt_char(v1_Power_Char, cmd)
                 except BleakError:
-                    print("Cannot send command to %s, it is probably NOT a base station", bs) # In case some random device with the same name as a BS gets caught by the scan
-            print("Disconnecting...")
+                    # In case some random device with the same name as a BS gets caught by the scan
+                    print( "Cannot send command to %s, it is probably NOT a base station", bs )
+            print("~ Disconnecting from %s... ~" % bs)
             await client.disconnect()
     except BleakError as e:
-        print("Unable to connect to Base Station: \n{}".format(e))
+        print("Unable to connect to device: \n{}".format(e))
 
 
 async def main():
     basestations = []
-    if len(saved_addrs) != 0:
+    tasks = []
+
+    # Skip scanning if BSes already saved
+    if len(saved_addrs):
         basestations = saved_addrs
+
+    # Scan for any nearby BSes
     else:
         print("Scanning...")
         devices = await BleakScanner.discover()
@@ -74,20 +82,25 @@ async def main():
         for d in devices:
             if d.name is not None and (d.name.startswith("HTC BS ") or d.name.startswith("LHB-")):
                 print("Found Base Station: %s" % d.name)
-                basestations.append(d) 
+                basestations.append(d)
         if len(basestations) == 0:
             print("No base stations found.")
             quit()
         print("~~~")
-        if args.save:
+        if args.save:  # Save BSes addresses to text file
             with open("basestations.txt", "w") as f:
                 addresses = list([bs.address for bs in basestations])
                 f.write(" ".join(addresses))
                 print("Saved %d addresses" % len(addresses))
-    for bs in basestations:
-        if not args.dryrun:
-            await powerState(bs, PowerState)
-        print("~~~")
+
+    # Connect to all BSes in parallel (might be stupid if you have >2 BSes, untested)
+    if not args.dryrun:
+        print( "Attempting to toggle power state" )
+        for bs in basestations:
+            # await powerState(bs, PowerState)
+            task = asyncio.create_task(powerState(bs, PowerState))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
     print("Done!")
 
 asyncio.run(main())
